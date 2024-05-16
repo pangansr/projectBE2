32df15b4c82bb062491e0da7ff8ac5548f2a48f5
thanhtam <thanhtamm733@gmail.com>
2024-05-16 23:20:45 +0700

    a

--- laravel/app/Models/OrderDetail.php
+++ laravel/app/Models/OrderDetail.php
@@ -8,8 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class OrderDetail extends Model
 {
     use HasFactory;
-<<<<<<< HEAD
-=======
 
     protected $table = 'order_detail'; // Tên bảng trong cơ sở dữ liệu
 
@@ -20,5 +18,5 @@ class OrderDetail extends Model
         'price',
     ];
     
->>>>>>> gfd
+
 }
